80db974e7701be2b73f108df39e7f8ea8c8f0842
chhwu <76684717+chhwu@users.noreply.github.com>
2021-01-08 13:30:31 +0800

    Update password_retry.py

--- password_retry.py
+++ password_retry.py
@@ -1,12 +1,12 @@
 password = 'a123456'
 chance = 3
 while chance > 0:
+    chance = chance - 1
     user = input('Please enter the password: ')
     if user == password:
         print('Login success!')
         break
     else:
-        chance = chance - 1
         if chance == 0:
             print('Fail to login.')
             break
